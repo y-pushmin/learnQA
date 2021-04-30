2832042a3d739472a85d994910d582a6c590493a
Пушмин Ярослав Павлович <j.pushmin@cft.ru>
2021-04-30 19:02:51 +0700

    [Test 3] Добавлено приватное поле и публичный метод в MainClass. Добавлен тест в MainClassTest

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,13 +5,19 @@ public class MainClassTest extends MainClass {
     @Test
     public void testGetLocalNumber() {
         int number = getLocalNumber();
-
-       Assert.assertTrue("Метод getLocalNumber возвращает не корректное число",number == 14);
+        Assert.assertTrue("Метод getLocalNumber возвращает не корректное число",number == 14);
     }
 
     @Test
     public void testGetClassNumber() {
         int number = getClassNumber();
         Assert.assertTrue("Метод getClassNumber возвращает число равное или меньше 45", number > 45);
+    }
+
+    @Test
+    public void testGetClassString() {
+        String str = getClassString();
+
+        Assert.assertTrue("Строка не содержит подстроку 'Hello' или 'hello'",str.contains("hello")||str.contains("Hello"));
     }
 }
